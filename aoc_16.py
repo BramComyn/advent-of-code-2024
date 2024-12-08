6ab38bbf555d56853439d97f13428376191281e8
Bram Comyn <bram.comyn@ugent.be>
2024-12-08 12:02:07 +0100

    refactor: anti code duplication gang

--- aoc_16.py
+++ aoc_16.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 from aoc_util import read_antenna_map, borders_safe
-from aoc_15 import target_regex, find_antennas
-from collections import defaultdict
 from itertools import combinations
-import re
+from aoc_15 import find_antennas
+
+def step_from(position, width, height, next_step):
+    positions = set()
+    while borders_safe(position, width, height):
+        positions.add(position)
+        position = next_step(position)
+    return positions
+
 
 def count_harmonic_antinode_positions(input_file="data/24-aoc-15.in"):
     """
@@ -23,15 +29,8 @@ def count_harmonic_antinode_positions(input_file="data/24-aoc-15.in"):
             first_position, second_position = position_pair
             dy, dx = second_position[0] - first_position[0], second_position[1] - first_position[1]
 
-            first_antinode_position = first_position
-            while borders_safe(first_antinode_position, width, height):
-                anti_nodes.add(first_antinode_position)
-                first_antinode_position = (first_antinode_position[0] - dy, first_antinode_position[1] - dx)
-
-            second_antinode_position = second_position
-            while borders_safe(second_antinode_position, width, height):
-                anti_nodes.add(second_antinode_position)
-                second_antinode_position = (second_antinode_position[0] + dy, second_antinode_position[1] + dx)
+            anti_nodes |= step_from(first_position, width, height, lambda position: (position[0] - dy, position[1] - dx))
+            anti_nodes |= step_from(second_position, width, height, lambda position: (position[0] + dy, position[1] + dx))
 
     return len(anti_nodes)
 
